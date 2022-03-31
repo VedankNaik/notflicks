be78a4234616b8af5b88336e22c46f3d2139cca7
Vedank Naik <vedankn01@gmail.com>
2022-03-31 18:38:56 +0530

    requests + dummy data

---
+++ src/requests.js
@@ -0,0 +1,17 @@
+//https://imdb-api.com/api
+// const apikey = "k_j46r01t4";
+
+const requests = {
+  search: "https://imdb-api.com/en/API/Search/k_j46r01t4/", //concat search expresion
+  top250movies: "https://imdb-api.com/en/API/Top250Movies/k_j46r01t4",
+  top250TV: "https://imdb-api.com/en/API/Top250TVs/k_j46r01t4",
+  popularMovies: "https://imdb-api.com/en/API/MostPopularMovies/k_j46r01t4",
+  popularTV: "https://imdb-api.com/en/API/MostPopularTVs/k_j46r01t4",
+  intheaters: "https://imdb-api.com/en/API/InTheaters/k_j46r01t4",
+  comingSoon: "https://imdb-api.com/en/API/ComingSoon/k_j46r01t4",
+  boxOffice: "https://imdb-api.com/en/API/BoxOffice/k_j46r01t4",
+  boxOfficeAllTime: "https://imdb-api.com/en/API/BoxOffice/k_j46r01t4",
+  ytTrailer: "https://imdb-api.com/en/API/YouTubeTrailer/k_j46r01t4/", //concat id
+};
+
+export default requests;
