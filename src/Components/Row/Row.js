2f6d9ff5247c8a19bce4a20acc35b9d94b90bb5d
Vedank Naik <vedankn01@gmail.com>
2022-03-31 18:37:54 +0530

    Added row component

---
+++ src/Components/Row/Row.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Row.css";
+import data from "../data.js";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
+function Row({ title, url }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(url);
+      setMovies(request.data.items);
+      return request;
+    }
+    fetchData();
+  }, [url]);
+
+  const opts = {
+    height: "500",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.title || "")
+        .then((url) => {
+          const urlParam = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParam.get("v"));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className="row__poster"
+            src={movie.image}
+            alt={movie.title}
+          />
+        ))}
+      </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+}
+
+export default Row;
