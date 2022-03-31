191135b72ab352b37bc276e7e834ad05ec578955
Vedank Naik <vedankn01@gmail.com>
2022-03-31 18:39:25 +0530

    Use components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import "./App.css";
+import Banner from "./Components/Banner/Banner";
+import NavBar from "./Components/NavBar/NavBar";
+import Row from "./Components/Row/Row";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
-      notflicks
+      <NavBar />
+      <Banner />
+      <Row title="Top 250 Movies" url={requests.top250movies} />
+      <Row title="Top 250 TV" url={requests.top250TV} />
+      <Row title="Popular Movies" url={requests.popularMovies} />
+      <Row title="Popular TV" url={requests.popularTV} />
+      <Row title="In Theaters" url={requests.intheaters} />
+      <Row title="Coming Soon" url={requests.comingSoon} />
+      <Row title="Box Office" url={requests.boxOffice} />
+      <Row title="Box Office All Time" url={requests.boxOfficeAllTime} />
     </div>
   );
 }
