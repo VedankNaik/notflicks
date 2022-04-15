f93bdbebd5d282a207758c37f3e77029c1541da5
Vedank Naik <vedankn01@gmail.com>
2022-04-15 21:09:10 +0530

    Protected routes

--- src/App.js
+++ src/App.js
@@ -3,50 +3,57 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Switch,
 } from "react-router-dom";
-import Banner from "./Components/Banner/Banner";
-import NavBar from "./Components/NavBar/NavBar";
-import Row from "./Components/Row/Row";
 import { Home, Browse, SignIn, SignUp } from "./pages";
-
 import * as ROUTES from "./constants/routes";
-import requests from "./requests";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
+import { useAuthListener } from "./hooks";
 
 function App() {
+  const { user } = useAuthListener();
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route exact path={ROUTES.HOME} element={<Home />} />
-          <Route exact path={ROUTES.BROWSE} element={<Browse />} />
-          <Route exact path={ROUTES.SIGN_IN} element={<SignIn />} />
-          <Route exact path={ROUTES.SIGN_UP} element={<SignUp />} />
+          <Route
+            exact
+            path={ROUTES.HOME}
+            element={
+              <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE}>
+                <Home />
+              </IsUserRedirect>
+            }
+          />
+          <Route
+            exact
+            path={ROUTES.SIGN_IN}
+            element={
+              <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE}>
+                <SignIn />
+              </IsUserRedirect>
+            }
+          />
+          <Route
+            exact
+            path={ROUTES.SIGN_UP}
+            element={
+              <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE}>
+                <SignUp />
+              </IsUserRedirect>
+            }
+          />
+          <Route
+            exact
+            path={ROUTES.BROWSE}
+            element={
+              <ProtectedRoute user={user}>
+                <Browse />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </div>
     </Router>
-
-    // <div className="App">
-    //   <Router>
-    //     <Jumbotron />
-    //     <Accordion />
-    //     <OptForm />
-    //     <Footer />
-
-    //     {/* Continue from constant routes */}
-
-    //     {/* <NavBar />
-    //   <Banner />
-    //   <Row title="Top 250 Movies" url={requests.top250movies} />
-    //   <Row title="Top 250 TV" url={requests.top250TV} />
-    //   <Row title="Popular Movies" url={requests.popularMovies} />
-    //   <Row title="Popular TV" url={requests.popularTV} />
-    //   <Row title="In Theaters" url={requests.intheaters} />
-    //   <Row title="Coming Soon" url={requests.comingSoon} />
-    //   <Row title="Box Office" url={requests.boxOffice} />
-    //   <Row title="Box Office All Time" url={requests.boxOfficeAllTime} /> */}
-    //   </Router>
-    // </div>
   );
 }
 
